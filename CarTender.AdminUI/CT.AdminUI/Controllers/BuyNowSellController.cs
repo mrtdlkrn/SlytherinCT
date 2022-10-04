6c49030ca61b2cdaffa008605179165012997be1
mrtdlkrn <dalkiran_mertkaya9876@hotmail.com>
2022-10-04 17:07:59 +0300

    BuyNowSellController added.

---
+++ CarTender.AdminUI/CT.AdminUI/Controllers/BuyNowSellController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CT.AdminUI.Controllers
+{
+    public class BuyNowSellController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("AddCar", ""); //todo: gelen sayfaya yönlendir 
+        }
+    }
+}
