103b066067bc9069cb54b040d9bad4005ee59834
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-10-04 21:34:15 +0300

    araba güncelleme sayfası hazırlandı

---
+++ CarTender.AdminUI/Entity/DTO/UpdateCarDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Entity.DTO
+{
+    public class UpdateCarDTO
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string BodyType { get; set; }
+        public string FuelType { get; set; }
+        public string Year { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
