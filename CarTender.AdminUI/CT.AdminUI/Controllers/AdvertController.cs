6b227aa72547a398e9c6a06ce75605edd253a3ef
mrtdlkrn <dalkiran_mertkaya9876@hotmail.com>
2022-10-05 10:17:39 +0300

    İlan bilgileri sayfası hazırlandı

---
+++ CarTender.AdminUI/CT.AdminUI/Controllers/AdvertController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CT.AdminUI.Controllers
+{
+    public class AdvertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewAdvert()
+        {
+            return View();
+        }
+    }
+}
