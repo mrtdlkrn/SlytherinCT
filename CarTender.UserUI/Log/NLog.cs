5492a99d0592dbff0e6096359eb0173b99f4417a
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-10-09 13:30:44 +0300

    Uı düzenlemeleri ve log işlemleri yapıldı

---
+++ CarTender.UserUI/Log/NLog.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using NLog;
+
+namespace Log
+{
+    public class NLog<T> : Logging<T> where T : class
+    {
+        private readonly Logger _logger = LogManager.GetLogger("databaseLogger");
+
+        public override void Log(T logData)
+        {
+            string strJson = JsonConvert.SerializeObject(logData);
+            _logger.Info(strJson);
+        }
+    }
+}
