d2d77a3d61a3511dc47eaa1195c1a659ebb999a7
Sarp İpekçi <sarpipekci@outlook.com>
2022-10-11 00:00:46 +0300

    Is Done!

---
+++ CarTender.AdminUI/CT.AdminUI/Controllers/PackageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CT.AdminUI.Controllers
+{
+    public class PackageController : Controller
+    {
+        #region Package Controller
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
