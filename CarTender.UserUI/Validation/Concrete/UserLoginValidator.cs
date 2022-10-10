03f3044acc9c5528476d1c3757e95bcd745a168f
yigitkygs <syktv2010@gmail.com>
2022-10-10 10:26:01 +0300

    Updated Validation

---
+++ CarTender.UserUI/Validation/Concrete/UserLoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.DTO;
+using FluentValidation;
+
+namespace Common.Validation.Concrete
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginDTO>
+    {
+        public UserLoginValidator()
+        {
+            RuleFor(user => user.Email).NotEmpty().MinimumLength(2).MaximumLength(30);
+            RuleFor(user => user.Password).NotEmpty().MinimumLength(2).MaximumLength(30);
+        }
+    }
+}
