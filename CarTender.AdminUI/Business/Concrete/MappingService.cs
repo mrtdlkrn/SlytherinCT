73d51327e84bf7f4038a340c261bec6eef1cf255
Burkay Akgül <burkayakgul@hotmail.com>
2022-10-12 15:54:18 +0300

    FlyPageler icin mapping servisde duzenleme yapildi dto ismini gore nesne bulma islemi mapping classinin icine eklendi,  startup'da injection islemi icin mappingservice singleton olarak eklendi.

--- CarTender.AdminUI/Business/Concrete/MappingService.cs
+++ CarTender.AdminUI/Business/Concrete/MappingService.cs
@@ -1,6 +1,9 @@
-﻿using Entity.DTO;
+﻿using Business.Abstract;
+using Entity.DTO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class MappingService
+    public class MappingService : IMappingService
     {
         public T GetModel<T>(IFormCollection formCollection) where T : class
         {
@@ -42,6 +45,37 @@ namespace Business.Concrete
             }
 
             return model;
+        }
+
+        public object GetModelByName(string modelName)
+        {
+            ///<summary>
+            /// Burada .net'in assembly kodlarindan veri tipleri arasindan tip adi bizim httpget ile aldigimiz model adina esit olan ilk tipin type bilgisini alir
+            /// Boylece gonderdigimiz modelName bilgisine gore veri tipini elde etmis oluruz.
+            /// </summary>
+            Type myModelType = System.AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).First(x => x.Name == modelName);
+
+
+            return Activator.CreateInstance(myModelType);
+        }
+
+        public object GetModelListByName(string modelName)
+        {
+            object model = GetModelByName(modelName);
+            Type modelType = model.GetType();
+
+            ///<summary>
+            /// Oluşturduğumuz model tipimizi listeye referans olarak veremedğimizden dolayı makegenerictype ile modelimizle bağlantılı bir liste
+            /// tipi oluşturmuş oluruz.
+            /// </summary>
+            Type customListType = typeof(List<>).MakeGenericType(modelType);
+
+            ///<summary>
+            ///List<GelenModelTipi> şeklinde bir liste instance'ı oluşturuyoruz
+            ///</summary>
+            object list = Activator.CreateInstance(customListType);
+            //var asdf = (IList)Activator.CreateInstance(customListType, model);
+            return list;
         }
     }
 }
