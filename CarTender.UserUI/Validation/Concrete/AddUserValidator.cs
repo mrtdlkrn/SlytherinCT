c1d791f6461349e7422d2e87ea9b716818a94968
yigitkygs <syktv2010@gmail.com>
2022-10-12 13:26:44 +0300

    Added new controllers and views to User UI

--- CarTender.UserUI/Validation/Concrete/AddUserValidator.cs
+++ CarTender.UserUI/Validation/Concrete/AddUserValidator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Entity.DTO;
@@ -10,10 +11,20 @@ namespace Common.Validation.Concrete
 {
     public class AddUserValidator : AbstractValidator<AddUserDTO>
     {
-        public AddUserValidator()
+        public AddUserValidator(AddUserDTO addUserDTO)
         {
             RuleFor(dto => dto.Name).NotEmpty().MinimumLength(2).MaximumLength(30);
             RuleFor(dto => dto.Surname).NotEmpty().MinimumLength(2).MaximumLength(20);
+
+
+            foreach(PropertyInfo p in addUserDTO.GetType().GetProperties())
+            {
+                if(p.PropertyType == typeof(string))
+                {
+                    
+                }
+
+            }
         }
     }
 }
