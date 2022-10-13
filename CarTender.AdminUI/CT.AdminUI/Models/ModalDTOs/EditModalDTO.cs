6ab3548e02698f1eeb1b1d9375182617dc72a624
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-10-13 18:47:48 +0300

    edit modal component hazırlandı

---
+++ CarTender.AdminUI/CT.AdminUI/Models/ModalDTOs/EditModalDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CT.AdminUI.Models.ModalDTOs
+{
+    public class EditModalDTO
+    {
+        public EditModalDTO(
+            string modalId,
+            string modalTitle, 
+            List<string> labelStrs, 
+            string saveBtnStr)
+        {
+            ModalID = modalId;
+            ModalTitle = modalTitle;
+            LabelStrs = labelStrs;
+            SaveBtnStr = saveBtnStr;
+        }
+        public string ModalID { get; set; }
+        public string ModalTitle { get; set; }
+        public List<string> LabelStrs{ get; set; }
+        public string SaveBtnStr { get; set; }
+    }
+}
