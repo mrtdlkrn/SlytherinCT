33c4be3ee2dedba29c37c7a036cc2a3fd5b4ca64
Sarp İpekçi <sarpipekci@outlook.com>
2022-10-16 16:14:18 +0300

    Fluent Validation Completed for Nuget Package

---
+++ CT.FluentValidation/CarTender.FluentValidation/VAL/UserVAL/Login-Register/UserLoginVAL.cs
@@ -0,0 +1,41 @@
+﻿using CarTender.FluentValidation.DTO.AdminDTO.Login;
+using CarTender.FluentValidation.DTO.UserDTO.Login_Register;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTender.FluentValidation.DAL.UserDAL.Login_Register
+{
+    public class UserLoginVAL : AbstractValidator<UserLoginDTO>
+    {
+        public UserLoginVAL()
+        {
+            #region User Username Login
+
+            RuleFor(x => x.Username)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("Kullanıcı adı alanı boş bırakılamaz.")
+                .MinimumLength(5).WithMessage("Kullanıcı adı alanı 5 karakterden küçük olamaz.")
+                .MaximumLength(20).WithMessage("Kullanıcı adı alanı 20 karakterden büyük olamaz.");
+
+            #endregion
+
+            #region User Password Login
+
+            RuleFor(x => x.Password)
+            .Cascade(CascadeMode.StopOnFirstFailure)
+            .NotEmpty().WithMessage("Şifre alanı boş bırakılamaz.")
+            .MinimumLength(8).WithMessage("Şifreniz 8 karakterden küçük olamaz.")
+            .MaximumLength(8).WithMessage("Şifreniz 8 karakterden büyük olamaz.")
+            .Matches(@"[A-Z]+").WithMessage("Şirenizde en az bir büyük karakter olmalıdır.")
+                .Matches(@"[a-z]+").WithMessage("Şifrenizde en az bir küçük karakter olmalıdır.")
+                .Matches(@"[0-9]+").WithMessage("Şifrenizde en az biri rakam olmalıdır.")
+                .Matches(@"[\!\?\*\.]+").WithMessage("Şifrenizde en az bir özel karakter olmalıdır(!? *.).");
+
+            #endregion
+        }
+    }
+}
