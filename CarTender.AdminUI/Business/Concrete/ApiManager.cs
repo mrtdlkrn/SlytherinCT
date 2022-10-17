751cd31ce51134ae3237e6489bd23f6b14358dde
AhmetOsmn <Ahmet.osmn@outlook.com>
2022-10-17 18:14:55 +0300

    API manager bugfix

--- CarTender.AdminUI/Business/Concrete/ApiManager.cs
+++ CarTender.AdminUI/Business/Concrete/ApiManager.cs
@@ -3,13 +3,14 @@ using Common.Abstract;
 using Core.Aspects.Autofac.Exception;
 using Core.Aspects.Autofac.Logging;
 using Core.Logging.Log4Net.Loggers;
+using Entity.DTO;
 using Entity.DTO.Auth;
 using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    [ExceptionLogAspect(typeof(FileLogger))]
-    [LogAspect(typeof(FileLogger))]
+    //[ExceptionLogAspect(typeof(FileLogger))]
+    //[LogAspect(typeof(FileLogger))]
 
     public class ApiManager : IApiService
     {
@@ -20,40 +21,36 @@ namespace Business.Concrete
             _baseAPIService = baseAPIService;
         }
 
-        public Task<bool> Delete(TokenDTO tokenDTO, string requestUrl)
+        public Task<ResponseDTO<T>> Delete<T>(TokenDTO tokenDTO, string requestUrl) where T : class
         {
-            return _baseAPIService.DELETE(tokenDTO, requestUrl);
+            return _baseAPIService.DELETE<T>(tokenDTO, requestUrl);
         }
 
-
-        public Task<DTO> Get<DTO>(TokenDTO tokenDTO, string requestUrl) where DTO : class
+        public Task<ResponseDTO<T>> Get<T>(TokenDTO tokenDTO, string requestUrl) where T : class
         {
-            return _baseAPIService.GET<DTO>(tokenDTO, requestUrl);
+            return _baseAPIService.GET<T>(tokenDTO, requestUrl);
         }
 
-        public Task<bool> Post<DTO>(TokenDTO tokenDTO, string requestUrl, DTO dto) where DTO : class
+        public Task<ResponseDTO<T>> Get<T, FilterDTO>(TokenDTO tokenDTO, string requestUrl, FilterDTO dto)
+            where T : class
+            where FilterDTO : class
+        {
+            return _baseAPIService.GET<T, FilterDTO>(tokenDTO, requestUrl, dto);
+        }
+
+        public Task<ResponseDTO<T>> Login<T>(T dto) where T : class
+        {
+            return _baseAPIService.LOGIN(dto);
+        }
+
+        public Task<ResponseDTO<T>> Post<T>(TokenDTO tokenDTO, string requestUrl, T dto) where T : class
         {
             return _baseAPIService.POST(tokenDTO, requestUrl, dto);
         }
 
-        public Task<string> Post<DTO>(string requestUrl, DTO dto) where DTO : class
-        {
-            return _baseAPIService.POST(requestUrl, dto);
-        }
-
-        public Task<bool> Put<DTO>(TokenDTO tokenDTO, string requestUrl, DTO dto) where DTO : class
+        public Task<ResponseDTO<T>> Put<T>(TokenDTO tokenDTO, string requestUrl, T dto) where T : class
         {
             return _baseAPIService.PUT(tokenDTO, requestUrl, dto);
-        }
-        public Task<DTO> GET<DTO, FilterDTO>(TokenDTO tokenDTO, string requestUrl, FilterDTO dto) where DTO : class where FilterDTO : class
-        {
-            return _baseAPIService.GET<DTO, FilterDTO>(tokenDTO, requestUrl, dto);
-        }
-
-
-        public void Test(string test)
-        {
-            System.Console.WriteLine("test");
         }
     }
 }
