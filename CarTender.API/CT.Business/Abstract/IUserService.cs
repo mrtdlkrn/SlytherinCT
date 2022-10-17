1729a650c3233d9d0b52d966ee3ee8030f10bc63
yigitkygs <syktv2010@gmail.com>
2022-10-18 00:24:41 +0300

    IUserService'te getirme, listeleme ve güncelleme metodları eklendi

--- CarTender.API/CT.Business/Abstract/IUserService.cs
+++ CarTender.API/CT.Business/Abstract/IUserService.cs
@@ -1,10 +1,16 @@
 ﻿using CT.Entities.Entities;
+using System.Collections.Generic;
 
 namespace CarTender.Business.Abstract
 {
     public interface IUserService
     {
         bool Add(User user);
+
+        List<User> GetAll();
+
         User GetByUserName(string userName);
+
+        bool Update(User user);
     }
 }
