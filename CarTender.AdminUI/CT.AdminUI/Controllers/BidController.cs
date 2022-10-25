6b8f08f9923c05c50b552acaa9f9e293ac05d455
Sarp İpekçi <sarpipekci@outlook.com>
2022-10-25 21:02:41 +0300

    Part 1

--- CarTender.AdminUI/CT.AdminUI/Controllers/BidController.cs
+++ CarTender.AdminUI/CT.AdminUI/Controllers/BidController.cs
@@ -2,19 +2,25 @@
 using CarTender.AdminUI.Controllers;
 using Common.Abstract;
 using Entity.DTO;
+using Entity.DTO.Auth;
 using Entity.DTO.Bid;
+using Entity.DTO.Car;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CT.AdminUI.Controllers
 {
     public class BidController : Controller
     {
         private readonly IApiService _apiService;
+        private readonly IDictionary<string, string> _routes;
 
-        public BidController(IApiService apiService)
+        public BidController(IApiService apiService, IApiRoutes routes)
         {
             _apiService = apiService;
+            _routes = routes.GetApiRoutes("Bid");
         }
         public IActionResult Index()
         {
@@ -25,15 +31,20 @@ namespace CT.AdminUI.Controllers
 
         [HttpGet]
         //GET: Bid Controller for Create New Bid
-        public IActionResult Create(CreateNewBidDTO createBidDTO)
+        public async Task<IActionResult> Create()
         {
-            return View();
+            TokenDTO tokenDTO = new TokenDTO();
+            var result = await _apiService.Get<CreateNewBidDTO>(tokenDTO, _routes[""]);
+            //todo : sayfaya veriler basılacak
+            return View(new CreateNewBidDTO());
         }
 
         [HttpPost]
         //POST: Bid Controller for Create New Bid
-        public IActionResult Create(int id)
+        public async Task<IActionResult> Create(CreateNewBidDTO dto)
         {
+            TokenDTO tokenDTO = new TokenDTO();
+            var result = await _apiService.Post(tokenDTO, _routes[""],dto);
             return View();
         }
 
